49738630509d6b8fd894b0521e5cf86be4458f65
mattspiro <matt@finedesigngroup.com>
2011-01-27 13:34:20 -0800

    removed index.html

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -33,8 +33,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content =>
-                          @base_title + " | Contact")
+                        :content => @base_title + " | Contact")
     end
   end
 
@@ -47,8 +46,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content =>
-                          @base_title + " | About")
+                        :content => @base_title + " | About")
     end
   end
 
@@ -61,8 +59,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                        :content =>
-                          @base_title + " | Help")
+                        :content => @base_title + " | Help")
     end
   end
 end
